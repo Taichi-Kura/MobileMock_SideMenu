1aa7f56acc43fedee35dfd60bd56b3ac5b97d9ad
Taichi Kuranari <67308225+Taichi-Kura@users.noreply.github.com>
2021-03-08 12:08:13 +0900

    MockApp変更

---
+++ SlideMenuController/SlideMenuController/SlideViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SlideViewController.swift
+//  SlideMenuController
+//
+//  Created by 倉成汰一 on 2021/03/04.
+//
+
+import UIKit
+import SlideMenuControllerSwift
+
+class SlideViewController: UIViewController {
+
+       override func viewDidLoad() {
+           super.viewDidLoad()
+       }
+
+}
---
+++ SlideMenuController/SlideMenuController/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  SlideMenuController
+//
+//  Created by 倉成汰一 on 2021/03/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var menuView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // メニューの位置を取得する
+        let menuPos = self.menuView.layer.position
+        // 初期位置を画面の外側にするため、メニューの幅の分だけマイナスする
+        self.menuView.layer.position.x = -self.menuView.frame.width
+        // 表示時のアニメーションを作成する
+        UIView.animate(
+            withDuration: 1.2,
+            delay: 0,
+            options: .curveEaseOut,
+            animations: {
+                self.menuView.layer.position.x = menuPos.x
+        },
+            completion: { bool in
+        })
+        
+    }
+
+    // メニューエリア以外タップ時の処理
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        for touch in touches {
+            if touch.view?.tag == 1 {
+                UIView.animate(
+                    withDuration: 0.2,
+                    delay: 0,
+                    options: .curveEaseIn,
+                    animations: {
+                        self.menuView.layer.position.x = -self.menuView.frame.width
+                },
+                    completion: { bool in
+                        self.dismiss(animated: true, completion: nil)
+                }
+                )
+            }
+        }
+    }
+    //http://swift.hiros-dot.net/?p=377
+
+}
